3ab4305c41dbaa8637cc755c27eb71ba3f413d92
alineraquel2001-collab <alineraquel2001@gmail.com>
2025-09-30 21:20:14 -0300

    calcular media - exemplo3

---
+++ 2025.09.29/media_ifrn3.py
@@ -0,0 +1,20 @@
+# Informando as notas como números inteiros
+intNota1 = int(input('Informe a nota da ETAPA 1:'))
+intNota2 = int(input('Informe a nota da ETAPA 2:'))
+
+# Calculando a média ponderada e arrendondando 
+# para o inteiro mais próximo
+intMedia = round( (intNota1*2 + intNota2*3) / 5)
+
+# Mostrando as notas e a média
+print(f'Nota da Etapa 1: {intNota1}')
+print(f'Nota da Etapa 2: {intNota2}')
+print(f'Média: {intMedia}')
+
+print('-------------------------------')
+if intMedia >= 60:
+    print('Aprovado!!!')
+elif intMedia >= 20:
+    print('Prova final!!!')   
+else:
+    print('Reprovado!!!')
