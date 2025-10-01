fb36e824c3539bd40a872974665abbd48ec9f6a3
alineraquel2001-collab <alineraquel2001@gmail.com>
2025-09-30 21:23:25 -0300

    Calcular raio

---
+++ 2025.09.24/calculo_raio.py
@@ -0,0 +1,10 @@
+#DIGITAR RAIO
+raio = input('Digite o raio do circulo:')
+
+raio = float(raio)
+
+pi = 3.14
+area = pi * (raio ** 2)
+
+#Imprimir valor da area
+print(f'A area do circulo é: {area: .2f}.')
