0f3694c83b6288967ccd778e65b3467f668a8ef5
alineraquel2001-collab <alineraquel2001@gmail.com>
2025-09-30 21:29:31 -0300

    Exibir dados - print()

---
+++ 2025.09.22/exemplo02.py
@@ -0,0 +1,10 @@
+
+nome = 'Aline';
+
+idade = 24;
+
+#Olá Aline, você tem 24 anos
+print(f'Olá' ,nome, ', você tem' ,idade, 'anos.');
+
+#Olá Aline, você tem 24 anos
+print(f'Olá {nome}, você tem {idade} anos.');
