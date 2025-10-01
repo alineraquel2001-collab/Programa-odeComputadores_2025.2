a9a69e33ced44f7224c128ab6e5850e3ccd95120
alineraquel2001-collab <alineraquel2001@gmail.com>
2025-09-30 21:21:38 -0300

    calcular media e frequencia

--- 2025.09.30/media_frequencia.py
+++ 2025.09.30/media_frequencia.py
@@ -12,7 +12,7 @@ intFaltas = int(input('Informe a quantidade de faltas do aluno: '))
 intMedia = round( (intNota1*2 + intNota2*3) / 5)
 
 # Calculando a frequência do aluno
-fltFrequencia = round(1 - (intFaltas / intCargaHoraria), 1)
+fltFrequencia = round( (1 - (intFaltas / intCargaHoraria)) * 100, 1)
 
 # Mostrando as notas e a média
 print(f'Nota da Etapa 1: {intNota1}')
@@ -22,10 +22,10 @@ print(f'Média: {intMedia}')
 # Mostrar a Frequência
 print(f'Total de Aulas: {intCargaHoraria}')
 print(f'Total de Faltas: {intFaltas}')
-print(f'Frequência: {fltFrequencia}')
+print(f'Frequência: {fltFrequencia}%')
 
 # Mostrando se o aluno está APROVADO, PROVA FINAL ou REPROVADO
-if (intMedia >= 60):
+if (intMedia >= 60) and (fltFrequencia>=75):
    print('APROVADO')
 elif (intMedia >= 20):
    print('PROVA FINAL')
