b9dc17c7e2913d79c09c69a42e320033c5df8167
alineraquel2001-collab <alineraquel2001@gmail.com>
2025-09-30 21:25:30 -0300

    media aritmetica

---
+++ 2025.09.24/media_aritmetica.py
@@ -0,0 +1,10 @@
+#MEDIA DOS VALORES
+print('Vamos calular a média aritmética!')
+
+valorA = int(input('Informe o primeiro valor:'))
+
+valorB = int(input('Informe o segundo valor:'))
+
+media = (valorA + valorB) / 2
+
+print(f'A média aritmética entre {valorA} e {valorB} é: {media}')
