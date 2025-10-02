3d4e3e195c9c912392add0e3a1241f48f3f968f9
alineraquel2001-collab <alineraquel2001@gmail.com>
2025-10-01 21:48:31 -0300

    Plano Cartesiano

--- 2025.10.01/exercicio_planoCartesiano.py
+++ 2025.10.01/exercicio_planoCartesiano.py
@@ -3,13 +3,7 @@ x = float(input("Digite a coordenada x: "))
 y = float(input("Digite a coordenada y: "))
 
 # Verifica a posição do ponto no plano cartesiano
-if x == 0 and y == 0:
-    print("O ponto está na origem.")
-elif x == 0:
-    print("O ponto está sobre o eixo y.")
-elif y == 0:
-    print("O ponto está sobre o eixo x.")
-elif x > 0 and y > 0:
+if  x > 0 and y > 0:
     print("O ponto está no quadrante 1.")
 elif x < 0 and y > 0:
     print("O ponto está no quadrante 2.")
@@ -17,3 +11,9 @@ elif x < 0 and y < 0:
     print("O ponto está no quadrante 3.")
 elif x > 0 and y < 0:
     print("O ponto está no quadrante 4.")
+elif x == 0 and y == 0:
+    print("O ponto está na origem.")
+elif y == 0:
+    print("O ponto está sobre o eixo x.")
+else:
+    print("O ponto está sobre o eixo y.")
