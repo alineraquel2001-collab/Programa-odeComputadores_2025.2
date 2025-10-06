d238ff9e78cf76c03dc14fb96b6e4d3a72e5261c
alineraquel2001-collab <alineraquel2001@gmail.com>
2025-10-06 20:00:35 -0300

    Calculando tratando exceções

--- 2025.10.06/calcularRaio2.py
+++ 2025.10.06/calcularRaio2.py
@@ -1,10 +1,37 @@
+'''
+	Você deve criar um programa que calcule o quociente entre dois números inteiros 
+	fornecidos pelo usuário. Durante o processo, é necessário tratar as seguintes exceções:
+
+		- Caso o segundo número seja zero, o programa deve exibir uma mensagem 
+		   informando que a divisão por zero não é permitida.
+
+		- Caso o usuário insira um valor que não seja um número inteiro, o programa deve 
+		  exibir uma mensagem solicitando que o usuário insira apenas números inteiros.
+
+	Instruções:
+
+		- Solicite ao usuário para inserir o primeiro número inteiro.
+
+		- Solicite ao usuário para inserir o segundo número inteiro.
+
+		- Calcule o quociente entre os dois números, tratando as exceções descritas acima.
+'''
 import sys
 
-raio = float(input('Informe o raio: '))
+try:
+   intDividendo = int(input('Informe o Dividendo: '))
+   intDivisor = int(input('Informe o Divisor..: '))
+   quociente = intDividendo / intDivisor
 
-if raio <= 0:
-   sys.exit('ERRO: O Raio deve ser positivo...')
+except ZeroDivisionError:
+ sys.exit('ERRO: Divisão por Zero não é permitida...')
 
-area = 3.1416 * raio ** 2
+except ValueError:
+ sys.exit('ERRO: Informe apenas números inteiros...')
 
-print(f'A área do círculo de raio = {raio} é {area:.2f}')
+except Exception as strErro:
+   sys.exit(f'EXCEÇÃO: {sys.exc_info()[0]} -> {strErro}')
+
+else:
+   print(f'{intDividendo} / {intDivisor} = {quociente:.2f}')
+
