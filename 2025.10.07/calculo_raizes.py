6f8be1da4b2fc17172b2a504b105a8c1698f712c
alineraquel2001-collab <alineraquel2001@gmail.com>
2025-10-07 20:50:18 -0300

    Calcular as raizes de uma equação do 2° grau

---
+++ 2025.10.07/calculo_raizes.py
@@ -0,0 +1,23 @@
+'''
+	Crie um programa em Python que calcule as raízes de uma equação do 2º grau.
+	
+	O programa deve:
+	
+		- Ler os valores de a, b e c como entrada;
+		
+		- Verificar se o valor de a é zero. Caso seja, a equação não será do 
+		  2º grau e o programa deve informar o usuário sobre isso e encerrar;
+		  
+		- Calcular o discriminante(delta) e, com base no valor de delta:
+			
+			- delta > 0 : a equação possui duas raízes reais distintas. 
+			  O programa deve calcular e exibir ambas as raízes;
+			  
+			- delta = 0 : a equação possui uma única raiz real. 
+			  O programa deve calcular e exibir a raiz única;
+			  
+			- delta < 0 : a equação não possui raízes reais. 
+			  O programa deve informar ao usuário que não existem raízes reais.
+			  
+		- Tratar as exceções devidas.
+'''
