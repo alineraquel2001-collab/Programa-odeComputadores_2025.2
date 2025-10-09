91ddc4ef29d5fc637e5bcd69f53f3ec4108bd171
Aline Raquel <alineraquel2001@gmail.com>
2025-10-08 21:33:06 -0300

    Update calculo_raizes.py

--- 2025.10.07/calculo_raizes.py
+++ 2025.10.07/calculo_raizes.py
@@ -19,6 +19,13 @@
 			- delta < 0 : a equação não possui raízes reais. 
 			  O programa deve informar ao usuário que não existem raízes reais.		
 '''
+# EQUAÇÃO DO SEGUNDO GRAU: ax² + bx + c = 0
+
+# Importa o módulo math, que fornece funções matemáticas, como math.sqrt() para calcular raízes quadradas.
+import math   
+
+# input(): Lê o valor digitado pelo usuário como texto (string).
+# float(): Converte a string para número decimal (ponto flutuante).
 # Solicita os coeficientes da equação ao usuário
 a = float(input("Informe o valor de a: "))
 b = float(input("Informe o valor de b: "))
@@ -28,12 +35,15 @@ c = float(input("Informe o valor de c: "))
 if a == 0:
     print("Não é uma equação do 2º grau. (a = 0)")
 else:
-    # Calcula o discriminante (delta)
+    # Caso a não seja = 0. Calcula o discriminante (delta) : Δ = b² - 4ac
+    # Vai determinar o número de raízes reais da equação 
     delta = b**2 - 4*a*c
     print(f"Delta (Δ) = {delta}")
 
     if delta > 0:
-        # Duas raízes reais distintas
+        # Se delta > 0 a equação tem duas raízes reais diferentes
+        # Para calcular as raizes usa Bhaskara: x1 = -b+raiz(Δ)/2a & x2 = -b-raiz(Δ)/2a
+        # math.sqrt(delta) calcula a raiz quadrada de delta.
         raiz1 = (-b + math.sqrt(delta)) / (2 * a)
         raiz2 = (-b - math.sqrt(delta)) / (2 * a)
         print("A equação possui duas raízes reais distintas:")
@@ -41,11 +51,12 @@ else:
         print(f"x2 = {raiz2}")
 
     elif delta == 0:
-        # Uma raiz real
+        # Se delta = 0, a equação tem uma única raiz real (dupla).
+        # Calcula a única raiz real
         raiz = -b / (2 * a)
         print("A equação possui uma raiz real (raiz dupla):")
         print(f"x = {raiz}")
 
     else:
-        # Delta negativo: sem raízes reais
+        # Caso delta < 0, não existem raízes reais, então o programa informa isso ao usuário.
         print("A equação não possui raízes reais (delta < 0).")
