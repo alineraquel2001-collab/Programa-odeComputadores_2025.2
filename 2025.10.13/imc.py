d327633b614442ddc9a89e2a1d603d92990a81aa
alineraquel2001-collab <alineraquel2001@gmail.com>
2025-10-13 20:32:14 -0300

    Calcular IMC

--- 2025.10.13/imc.py
+++ 2025.10.13/imc.py
@@ -13,7 +13,8 @@
       - 18.5 a 24.9: Peso ideal
       - 25.0 a 29.9: Sobrepeso
       - 30.0 a 34.9: Obesidade Grau I
-      - Acima de 35.0: Obesidade Grau II
+      - 35.0 a 39.9: Obesidade Grau II
+      - 40.0 ou mais: Obesidade Grau III
 
    O programa deve tratar entradas inválidas (não numéricas) e 
    também verificar se a altura informada é maior que zero para evitar 
@@ -42,8 +43,10 @@ def calcular_imc():
             classificacao = "Sobrepeso"
         elif imc < 35.0:
             classificacao = "Obesidade Grau I"
+        elif imc < 39.9:
+            classificacao = "Obesidade Grau Ii"
         else:
-            classificacao = "Obesidade Grau II"
+            classificacao = "Obesidade Grau IIi"
 
         print(f"Classificação: {classificacao}")
 
@@ -52,3 +55,31 @@ def calcular_imc():
 
 # Executa o programa
 calcular_imc()
+
+'''
+import sys
+
+# Entrada de dados com tratamento de erro
+try:
+   fltPeso   = float(input('Informe o seu peso em quilogramas (ex: 70.5): '))
+   fltAltura = float(input('Informe a sua altura em metros (ex: 1.75)...: '))
+# Tratamento de erro para valores não numéricos
+except ValueError:
+   sys.exit('ERRO: Você deve digitar um valor numérico.')
+# Tratamento de erro para outros tipos de erro
+except Exception as strErro:
+   sys.exit(f'ERRO: {strErro}')
+# Cálculo do IMC caso tenha passado pelas validações
+else:
+   # Verifica se a altura é menor ou igual a zero
+   if fltAltura <= 0:
+      sys.exit('ERRO: A altura deve ser maior que zero.')
+
+   # Verifica se o peso é menor ou igual a zero
+   if fltPeso <= 0:
+      sys.exit('ERRO: O peso deve ser maior que zero.')
+
+if:
+    float_peso <= 0 or float_altura <= 0
+    sys.exit('ERRO: Info de peso e altura devem ser maiores que Zero.')
+'''
