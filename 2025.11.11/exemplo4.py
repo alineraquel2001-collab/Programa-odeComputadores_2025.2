bb69eff0e513e4369bae29aa0244797842beb49c
alineraquel2001-collab <alineraquel2001@gmail.com>
2025-11-11 21:44:42 -0300

    Exemplo 4

---
+++ 2025.11.11/exemplo4.py
@@ -0,0 +1,19 @@
+import sys
+
+strTexto = input ('Digite uma palavra: ')
+
+if ' ' in strTexto:
+    sys.exit('Você digitou mais de uma palavra!')
+
+#Inverter palavra
+
+strTextoInvertido = strTexto[::-1]
+print(strTexto)
+print(strTextoInvertido)
+if strTexto.lower() == strTextoInvertido.lower():
+    print('Palindromo')
+else:
+    print('Não é palindromo')
+
+
+#lower - converter todas as letras de uma string para minúsculas.
