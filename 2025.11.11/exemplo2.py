4bdb7dda0011f74a53f123f54b754e9f26ea6de9
alineraquel2001-collab <alineraquel2001@gmail.com>
2025-11-11 21:03:48 -0300

    Exemplo 2

---
+++ 2025.11.11/exemplo2.py
@@ -0,0 +1,25 @@
+intQuant = 5
+
+#USANDO FOR
+for i in range (1, intQuant+1):
+    print ('*' * i)
+for i in range (intQuant - 1, 0, -1):
+     print ('*' * i)   
+
+''' 
+ #USANDO WHILE 
+
+i = 1
+while i <= intQuant:
+    print('*' * i)
+    i += 1
+
+i = intQuant - 1
+while i > 0:
+    print('*' * i)
+    i -= 1
+
+'''
+
+ 
+ 
