adeee5d22e0d8184199099ac4c2b3f7a5225a94e
alineraquel2001-collab <alineraquel2001@gmail.com>
2025-12-15 19:36:09 -0300

    exemplo 2

---
+++ 2025.12.15/exemplo2.py
@@ -0,0 +1,4 @@
+'''
+   Fazer um programa que leia o arquivo valores_1.txt, que contém números inteiros,
+   um por linha, e calcule a soma desses números. O programa deve exibir o resultado na tela.
+'''
