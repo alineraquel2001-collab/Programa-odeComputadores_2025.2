388592f44ac1c1492075f098a684c1d0e32e7bfd
alineraquel2001-collab <alineraquel2001@gmail.com>
2025-12-15 19:35:17 -0300

    exemplo

---
+++ 2025.11.24/list.py
@@ -0,0 +1,6 @@
+listNumeros = list()
+intLimite = 100
+
+for intValor in range (intLimite):
+    listNumeros.append(intValor)
+     
---
+++ 2025.12.15/exemplo.py
@@ -0,0 +1,4 @@
+'''
+   Fazer um programa que leia o conteúdo do arquivo resumo_lotr.txt e imprima na tela.
+'''
+
