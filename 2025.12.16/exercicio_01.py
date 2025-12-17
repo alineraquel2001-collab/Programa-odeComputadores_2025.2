5d509bc303d186a31260adfba7086023561cc15e
alineraquel2001-collab <alineraquel2001@gmail.com>
2025-12-16 21:28:32 -0300

    Exercicio

--- 2025.12.16/exercicio_01.py
+++ 2025.12.16/exercicio_01.py
@@ -14,3 +14,77 @@
 
    Escreva os resultados em um arquivo chamado resultados.txt.
 '''
+
+# Importando o módulo os para manipulação de caminhos de arquivos
+import os, sys, statistics
+
+# Obtendo o diretório do arquivo atual
+strDiretorio = os.path.dirname(__file__)
+
+# Definindo o nome do arquivo a ser lido
+strNomeArquivo = 'valores_1.txt'
+
+try:
+   # Tentando abrir o arquivo no modo de leitura
+   arqLeitura = open(f'{strDiretorio}/{strNomeArquivo}', 'r', encoding='utf-8')
+except FileNotFoundError:
+   # Tratando o erro caso o arquivo não seja encontrado
+   sys.exit(f'ERRO: Arquivo "{strNomeArquivo}" não encontrado!')
+except Exception as e:
+   # Tratando outros erros genéricos
+   sys.exit(f'ERRO: {e}')
+else:
+   # Criando uma lista que irá guardar os números
+   lstNumeros = list()
+
+   # Lendo o conteúdo do arquivo
+   while True:
+      # Lendo uma linha do arquivo e removendo caracteres 
+      # de espaço em branco e caractere de nova linha (\n)
+      strLinha = arqLeitura.readline().strip()
+
+      # Verificando se chegou ao final do arquivo
+      if not strLinha: break
+
+      # Convertendo a linha lida para inteiro
+      try:
+         intValor = int(strLinha)
+      except ValueError:
+         # Tratando o erro caso a conversão falhe
+         print(f'ERRO: Valor inválido "{strLinha}" ignorado!')
+         continue
+      except Exception as e:
+         # Tratando outros erros genéricos
+         print(f'ERRO: {e} ao processar o valor "{strLinha}" ignorado!')
+         continue
+      
+      # Adicionando o valor lido convertido em inteiro à lista
+      lstNumeros.append(intValor)
+
+   # Fechando o arquivo após a leitura
+   arqLeitura.close()
+
+intSoma = sum(lstNumeros)
+flttMedia = intSoma/len(lstNumeros)
+
+
+
+
+fltMediana = statistics.median(lstNumeros)
+fltVariancia = statistics.variance(lstNumeros)
+fltDesvioPadrao = statistics.stdev(lstNumeros)
+
+try:
+     arqResultados = open(f'{strDiretorio}/resultados.txt', 'w')	
+
+except Exception as e:
+      sys.exit(f'ERRO: {e}')    
+else:
+      arqResultados.write(f'Soma.........: {intSoma}\n')
+      arqResultados.write(f'Média........: {fltMedia}\n')
+      arqResultados.write(f'Mediana......: {fltMediana}\n')
+      arqResultados.write(f'Variância....: {fltVariancia}\n')
+      arqResultados.write(f'Desvio Padrão: {fltDesvioPadrao}\n')
+
+      # Fechando o arquivo de resultados após a escrita
+arqResultados.close()
