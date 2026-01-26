522ec83f3b13816bd47da7f1f1324a9c85c0d134
alineraquel2001-collab <alineraquel2001@gmail.com>
2026-01-26 20:28:13 -0300

    Exercicio

--- 2026.01.26/times_brasileirao.py
+++ 2026.01.26/times_brasileirao.py
@@ -3,7 +3,7 @@
    dicionário com os nomes dos times como chaves e um dicionário
    como valor contendo 'Ano', 'Vitórias', 'Empates', 'Derrotas', 
    'Gols Pró', 'Gols Contra', 'Cartões Amarelos' e 'Cartões Vermelhos'.
-'''
+
 import os, sys, json
 
 strDiretorio   = os.path.dirname(__file__)
@@ -55,3 +55,60 @@ else:
 
    # Exibir o dicionário resultante
    print(dictClassificacao)
+
+   '''
+import os, sys, json
+
+strDiretorio   = os.path.dirname(__file__)
+strNomeArquivo = f'{strDiretorio}/Brasileirao_Serie_A.csv'
+
+intAno = 2023
+
+try:
+   arqEntrada = open(strNomeArquivo, 'r', encoding='utf-8')
+except FileNotFoundError:
+   sys.exit('Erro: O arquivo não foi encontrado.')
+except Exception as e:
+   sys.exit(f'Erro ao abrir o arquivo: {e}')
+else:
+   # Ler a primeira linha para obter as chaves do dicionário interno
+   lstChaves = arqEntrada.readline().strip().split(';')
+   
+   # Remover a chave 'Time' que não será usada no dicionário interno
+   lstChaves.pop(0)
+
+   # Inicializar o dicionário principal
+   dictTimes= dict()
+
+   while True:
+      # Lendo os dados de cada time
+      strLinha = arqEntrada.readline().strip()
+      
+      # Verificar se chegou ao final do arquivo
+      if not strLinha: break
+
+      # Dividir a linha em uma lista de dados
+      lstDados = strLinha.split(';')
+      
+      # Chave de dictClassificacao -> Nome do time
+      strTime = lstDados[0]
+      
+      # Eliminar o nome do time da lista de dados
+      lstDados.pop(0)
+
+      # Criar o dicionário de cada time associando chaves e valores
+      dictInfoTime = dict(zip(lstChaves, lstDados))
+
+      # Adicionar o dicionário do time ao dicionário principal
+      dictTimes[strTime] = dictInfoTime
+
+   # Fechar o arquivo após a leitura
+   arqEntrada.close()
+
+   # Convertendo o dicionário python para o formato JSON nativo/
+   dictTimes= json.dumps(dictTimes, ensure_ascii=False)
+
+   # Exibir o dicionário resultante
+   print(dictTimes)
+
+   print(len(dictTimes), 'times no dicionário.')
